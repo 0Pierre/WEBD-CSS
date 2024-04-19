db081ae91b568c22bcc3f361102e2fcc4e960aab
0Pierre <151689805+0Pierre@users.noreply.github.com>
2024-04-18 20:01:35 -0400

    comment

--- Assignments/Lab4_Francisco/controllers/animalController.js
+++ Assignments/Lab4_Francisco/controllers/animalController.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.render('home', { pageTitle: 'Home' });
-  });
+// app.get('/', (req, res) => {
+//     res.render('home', { pageTitle: 'Home' });
+//   });
   
-  app.get('/animal-list', (req, res) => {
-    res.render('animal-list', { pageTitle: 'Animal List' });
-  });
+//   app.get('/animal-list', (req, res) => {
+//     res.render('animal-list', { pageTitle: 'Animal List' });
+//   });
   
-  app.get('/entry-form', (req, res) => {
-    res.render('entry-form', { pageTitle: 'Entry Form' });
-  });
+//   app.get('/entry-form', (req, res) => {
+//     res.render('entry-form', { pageTitle: 'Entry Form' });
+//   });
   
